569814f2e010e9f0baf3350984df73ddf77bac31
anhhaokudo <hao.paul.nguyen@gmail.com>
2022-05-30 21:49:26 -0700

    clean code

--- main.py
+++ main.py
@@ -70,34 +70,21 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     print(opt)
 
-    source = ("Image Detection", "Video Detection")
-    source_index = st.sidebar.selectbox("Select Input", range(
-        len(source)), format_func=lambda x: source[x])
+    source = ("Image Detection")
+   
 
-    if source_index == 0:
-        uploaded_file = st.sidebar.file_uploader(
-            "Image", type=['png', 'jpeg', 'jpg'])
-        if uploaded_file is not None:
-            is_valid = True
-            with st.spinner(text='Loading...'):
-                st.sidebar.image(uploaded_file)
-                picture = Image.open(uploaded_file)
-                picture = picture.save(f'data/images/{uploaded_file.name}')
-                opt.source = f'data/images/{uploaded_file.name}'
-        else:
-            is_valid = False
+    uploaded_file = st.sidebar.file_uploader(
+        "Image", type=['png', 'jpeg', 'jpg'])
+    if uploaded_file is not None:
+        is_valid = True
+        with st.spinner(text='Loading...'):
+            st.sidebar.image(uploaded_file)
+            picture = Image.open(uploaded_file)
+            picture = picture.save(f'data/images/{uploaded_file.name}')
+            opt.source = f'data/images/{uploaded_file.name}'
     else:
-        uploaded_file = st.sidebar.file_uploader("Video", type=['mp4'])
-        if uploaded_file is not None:
-            is_valid = True
-            with st.spinner(text='Loading...'):
-                st.sidebar.video(uploaded_file)
-                with open(os.path.join("data", "videos", uploaded_file.name), "wb") as f:
-                    f.write(uploaded_file.getbuffer())
-                opt.source = f'data/videos/{uploaded_file.name}'
-        else:
-            is_valid = False
-
+        is_valid = False
+    
     if is_valid:
         print('valid')
         if st.button('Start Detection'):
@@ -107,23 +94,12 @@ if __name__ == '__main__':
                 view_img=True,
                 save_txt=True
             )
+           
+            with st.spinner(text='Preparing Images'):
+                for img in os.listdir(get_detection_folder()):
+                    if img == 'labels':
+                        continue
+                    st.image(str(Path(f'{get_detection_folder()}') / img))
 
-
-
-
-            #detect(opt)
+                st.balloons()
             
-            if source_index == 0:
-                with st.spinner(text='Preparing Images'):
-                    for img in os.listdir(get_detection_folder()):
-                        if img == 'labels':
-                            continue
-                        st.image(str(Path(f'{get_detection_folder()}') / img))
-
-                    st.balloons()
-            else:
-                with st.spinner(text='Preparing Video'):
-                    for vid in os.listdir(get_detection_folder()):
-                        st.video(str(Path(f'{get_detection_folder()}') / vid))
-
-                    st.balloons()
